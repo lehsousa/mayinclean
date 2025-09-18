91840cbd38191c47615afc6d9ffc81a7f3fb1cad
Leandro Sousa <leonascurvas@gmail.com>
2025-09-17 21:55:18 -0300

    ajustar slogan, ajustar botão para o whatsapp

--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -1,5 +1,10 @@
 import './Home.css';
 
+  const phoneNumber = '5511990096260';
+  const message = "Olá vim pelo google e gostaria de um orçamento.";
+  const encodedMessage = encodeURIComponent(message);
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
 const Home = () => {
   return (
     <section id="home" className="home-section">
@@ -21,9 +26,17 @@ const Home = () => {
       {/* Conteúdo da seção que ficará sobre o vídeo */}
       <div className="home-content container">
         <div className="home-text">
-          <h1>Sua casa limpa, <span>sua família protegida.</span></h1>
+          
+          <h1>"Porque cuidar de estofados, <span>é cuidar de vidas"</span></h1>
           <p>Devolvemos a aparência de novo e o bem-estar ao seu ambiente com a mais completa higienização de estofados.</p>
-          <a href="#contact" className="home-cta">Solicite um Orçamento Agora</a>
+          <a 
+        href={whatsappUrl} 
+        className="home-cta"
+        target="_blank" // Abre em uma nova aba
+        rel="noopener noreferrer" // Boas práticas de segurança
+      >
+        Solicite um Orçamento Agora
+      </a>
         </div>
         <div className="home-image">
            <img src="/assets/logoverdetransparente.png" alt="Mayin Clean Logo" />
